ec9715d8a817176ff0a69c8873639e593b60caad
tvkeprof <wefousoul@gmail.com>
2025-01-16 18:30:06 +0800

    s

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
   const [filteredData, setFilteredData] = useState([]);
   const [weatherData, setWeatherData] = useState({});
   const data = [countriesData];
+  console.log(weatherData)
 
   const getCountries = async () => {
     try {
@@ -38,14 +39,32 @@ function App() {
         `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${selectedCity}`
       );
       const result = await response.json();
-      const formattedDate = new Date(result.location.localtime).toLocaleDateString("en-US", {
+      const dayWeather = result.forecast.forecastday[0].day;
+      const nightWeather = result.forecast.forecastday[0].hour.filter(
+        (hour) => hour.is_day === 0
+      );
+      const formattedDate = new Date(
+        result.location.localtime
+      ).toLocaleDateString("en-US", {
         year: "numeric",
         month: "long",
         day: "numeric",
       });
-      setWeatherData({...result, formattedDate});
+
+      setWeatherData({ ...result, formattedDate,
+        day:{
+          temp: dayWeather.avgtemp_c,
+          condition: dayWeather.condition.text,
+          icon: dayWeather.condition.icon,
+        },
+        night:{
+          temp: nightWeather[0]?.temp_c || "N/A",
+          condition: nightWeather[0]?.condition.text || "N/A",
+          icon: nightWeather[0]?.condition.icon || "./default-night-icon.png",
+        },
+       });
+       setWeatherData(result)
       console.log(result);
-      
     } catch (error) {
       console.log(error);
     }
@@ -57,13 +76,45 @@ function App() {
     setFilteredData([]);
   };
 
-  const onChange = (event) => {
-    setSearchValue(event.target.value);
-    const filtered = allCities
-      .filter((el) => el.toLowerCase().startsWith(searchValue.toLowerCase()))
-      .slice(0, 5);
+  const getWeatherimage =()=>{
+    const conditionText = weatherData.forecast?.forecastday[0]?.day?.condition?.text
+    if (conditionText === "Sunny"){
+      return "/icon.png"
+    } else return "/snow.png"
+  }
 
-    setFilteredData(filtered);
+  //   const mood = {
+  //      sunny: "./icon.png",
+  //      cloud: "./cloud.png",
+  //      wind: "./wind.png",
+  //      rain: "./raind.png",
+  //      snow: "./snow.png",
+  //      thunderstorm: "./thunderstorm.png",
+  //      nightClear: "./nightClear.png",
+  //      nightCloud: "./nightCloud.png",
+  //      nightWind: "./nightWind.png",
+  //      nightRain: "./nightRain.png",
+  //      nightSnow: "./moon.png",
+  //      nightThunderstorm: "./nightThunderstorm",
+  //   };
+  // const dayIcon = mood[weatherData?.day?.condition] || "./cloud.png";
+  // const nightIcon = mood[weatherData?.night?.condition] || "./nightClear.png";
+
+
+
+  const onChange = (event) => {
+    const value = event.target.value;
+    setSearchValue(value);
+  
+    if (value.trim() === "") {
+      setFilteredData([]);
+    } else {
+      const filtered = allCities
+        .filter((el) => el.toLowerCase().startsWith(value.toLowerCase()))
+        .slice(0, 5);
+  
+      setFilteredData(filtered);
+    }
   };
   useEffect(() => {
     getCountries();
@@ -77,27 +128,39 @@ function App() {
   return (
     <>
       <div className="w-full h-screen bg-sky-400 flex relative justify-center">
-        <input
-          className="border border-black absolute p-2 top-[100px] z-10 w-[500px] h-[80px]"
-          value={searchValue}
-          onChange={onChange}
-          placeholder="Search"
+        <div className=" absolute flex top-[100px]">
+          <div className="flex rounded-[50px] bg-[white] w-[400px] gap-[20px] items-center z-10 px-[5px] py-[5px]">
+          <Search />
+          <input
+            className=" top-[100px] z-10  text-[30px] rounded-[50px] border-none outline-none p-[5px]"
+            value={searchValue}
+            onChange={onChange}
+            placeholder="Search"
+          />
+            
+          
+          </div>
 
-          /> 
-          <Search/>
-        <div className="absolute top-[150px] z-10 bg-white text-black">
-          {filteredData.map((el) => (
-            <p className="p-2 cursor-pointer hover:bg-gray-200" onClick={() => handleClickCtiy(el)} key={el}>
-              {el}
-            </p>
-          ))}
+          <div className="absolute top-[70px] z-10 text-black w-[400px] rounded-[30px] mt-2.5 rounded-3xl bg-white/80 shadow-lg backdrop-blur-md">
+            {filteredData.map((el) => (
+              <p
+                className="p-2 cursor-pointer hover:bg-gray-200 rounded-[30px]"
+                onClick={() => handleClickCtiy(el)}
+                key={el}
+              >
+                {el}
+              </p>
+            ))}
+          </div>
         </div>
         <div className="w-[170px] h-[170px] absolute rounded-full bg-orange-500 top-[100px] left-[300px]"></div>
         <div className="w-1/2 h-screen bg-gray-200 flex justify-center items-center">
           <div className="w-[330px] h-[800px] bg-white/50z-10 flex justify-center  rounded-[50px] px-[40px] py-[60px]  backdrop-blur-md z-10">
             <div className="flex">
               <div>
-                <p className="text-gray-400">{weatherData?.formattedDate || "Loading..."}</p>
+                <p className="text-gray-400">
+                  {weatherData?.formattedDate || "Loading..."}
+                </p>
                 <h1 className="h-12 text-5xl font-extrabold text-gray-900">
                   {selectedCity}
                 </h1>
@@ -106,16 +169,16 @@ function App() {
             </div>
             <img
               className="w-[250px] h-[250px] absolute top-[130px] "
-              src="./icon.png"
+              src={getWeatherimage()}
               alt=""
             />
             <h1 className="text-transparent bg-clip-text font-extrabold text-[110px] -mt-10 bg-gradient-to-b from-black to-white absolute top-[370px]">
-            {weatherData?.current?.temp_c
-              ? `${weatherData.current.temp_c}˚`
-              : "Loading..."}
+              {weatherData?.current?.temp_c
+                ? `${weatherData.current.temp_c}˚`
+                : "Loading..."}
             </h1>
             <p className="font-extrabold mb-12 h-6 absolute top-[480px] left-[50px] text-orange-500">
-            {weatherData?.current?.condition?.text || "Loading..."}
+              {weatherData?.current?.condition?.text || "Loading..."}
             </p>
 
             <div className="flex absolute top-[540px] gap-[30px]">
@@ -131,23 +194,23 @@ function App() {
           <div className="w-[330px] h-[800px] bg-gray-700/50z-10 flex justify-center rounded-[50px] backdrop-blur-md z-10 px-[40px] py-[60px]">
             <div className="flex">
               <div>
-                <p className="text-gray-400">January14,2025</p>
+                <p className="text-gray-400">{weatherData?.formattedDate || "Loading..."}</p>
                 <h1 className="h-12 text-5xl font-extrabold text-white ">
-                  Ulaanbaatar
+                {selectedCity}
                 </h1>
               </div>
               <Location />
             </div>
             <img
               className="w-[250px] h-[250px] absolute top-[130px] "
-              src="./moon.png"
+              src={getWeatherimage}
               alt=""
             />
             <h1 className="text-transparent bg-clip-text font-extrabold text-[110px] -mt-10 bg-gradient-to-b from-black to-white absolute top-[370px]">
-              -26˚
+            {weatherData?.night?.temp ? `${weatherData.night.temp}˚` : "Loading..."}
             </h1>
             <p className="font-extrabold mb-12 h-6 absolute top-[480px] left-[50px] text-sky-300">
-              Snow
+            {weatherData?.night?.condition || "Loading..."}
             </p>
 
             <div className="flex absolute top-[540px] gap-[30px]">
